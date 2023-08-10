54c261b25862f648741489a77e669923c651d4b2
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-08-10 10:22:07 -0400

    Update Drawer.java

--- src/Drawer.java
+++ src/Drawer.java
@@ -17,38 +17,38 @@ public class Drawer extends JPanel implements ActionListener {
     int camX = 0;
     int camY = 0;
 
-    public int[][][] objects = new int[256][4][3];
+    public int[][] objects = new int[4096][8];
 
-    public BufferedImage[] decals = new BufferedImage[256];
+    public BufferedImage[] decals = new BufferedImage[4096];
 
-    public String[] strings = new String[256];
+    public String[] strings = new String[4096];
 
-    public int[][][] UIObjects = new int[256][4][3];
+    public int[][] UIObjects = new int[4096][8];
 
-    public BufferedImage[] UIDecals = new BufferedImage[256];
+    public BufferedImage[] UIDecals = new BufferedImage[4096];
 
-    public String[] UIStrings = new String[256];
+    public String[] UIStrings = new String[4096];
 
     public void CreateObj(int objid, int X, int Y, int width_X2, int height_Y2, int shape, int red, int green, int blue) {
-        objects[objid][0][0] = X;
-        objects[objid][0][1] = Y;
-        objects[objid][1][0] = width_X2;
-        objects[objid][1][1] = height_Y2;
-        objects[objid][2][0] = red;
-        objects[objid][2][1] = green;
-        objects[objid][2][2] = blue;
-        objects[objid][3][0] = shape;
+        objects[objid][0] = X;
+        objects[objid][1] = Y;
+        objects[objid][2] = width_X2;
+        objects[objid][3] = height_Y2;
+        objects[objid][4] = red;
+        objects[objid][5] = green;
+        objects[objid][6] = blue;
+        objects[objid][7] = shape;
     }
 
     public void DeleteObj(int objid) {
-        objects[objid][0][0] = 0;
-        objects[objid][0][1] = 0;
-        objects[objid][1][0] = 0;
-        objects[objid][1][1] = 0;
-        objects[objid][2][0] = 0;
-        objects[objid][2][1] = 0;
-        objects[objid][2][2] = 0;
-        objects[objid][3][0] = 0;
+        objects[objid][0] = 0;
+        objects[objid][1] = 0;
+        objects[objid][2] = 0;
+        objects[objid][3] = 0;
+        objects[objid][4] = 0;
+        objects[objid][5] = 0;
+        objects[objid][6] = 0;
+        objects[objid][7] = 0;
     }
 
     public void ChangeDecal(int objid, String imgDir) {
@@ -66,25 +66,25 @@ public class Drawer extends JPanel implements ActionListener {
     }
 
     public void CreateUIObj(int UIObjID, int X, int Y, int width_X2, int height_Y2, int shape, int red, int green, int blue) {
-        UIObjects[UIObjID][0][0] = X;
-        UIObjects[UIObjID][0][1] = Y;
-        UIObjects[UIObjID][1][0] = width_X2;
-        UIObjects[UIObjID][1][1] = height_Y2;
-        UIObjects[UIObjID][2][0] = red;
-        UIObjects[UIObjID][2][1] = green;
-        UIObjects[UIObjID][2][2] = blue;
-        UIObjects[UIObjID][3][0] = shape;
+        UIObjects[UIObjID][0] = X;
+        UIObjects[UIObjID][1] = Y;
+        UIObjects[UIObjID][2] = width_X2;
+        UIObjects[UIObjID][3] = height_Y2;
+        UIObjects[UIObjID][4] = red;
+        UIObjects[UIObjID][5] = green;
+        UIObjects[UIObjID][6] = blue;
+        UIObjects[UIObjID][7] = shape;
     }
 
     public void DeleteUIObj(int UIObjID) {
-        UIObjects[UIObjID][0][0] = 0;
-        UIObjects[UIObjID][0][1] = 0;
-        UIObjects[UIObjID][1][0] = 0;
-        UIObjects[UIObjID][1][1] = 0;
-        UIObjects[UIObjID][2][0] = 0;
-        UIObjects[UIObjID][2][1] = 0;
-        UIObjects[UIObjID][2][2] = 0;
-        UIObjects[UIObjID][3][0] = 0;
+        UIObjects[UIObjID][0] = 0;
+        UIObjects[UIObjID][1] = 0;
+        UIObjects[UIObjID][2] = 0;
+        UIObjects[UIObjID][3] = 0;
+        UIObjects[UIObjID][4] = 0;
+        UIObjects[UIObjID][5] = 0;
+        UIObjects[UIObjID][6] = 0;
+        UIObjects[UIObjID][7] = 0;
     }
 
     public void ChangeUIDecal(int UIObjID, String imgDir) {
@@ -105,42 +105,42 @@ public class Drawer extends JPanel implements ActionListener {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
         for (int i = 0; i < objects.length; i++) {
-            g2.setColor(new Color(objects[i][2][0], objects[i][2][1], objects[i][2][2]));
-            switch (objects[i][3][0]) {
+            g2.setColor(new Color(objects[i][4], objects[i][5], objects[i][6]));
+            switch (objects[i][7]) {
                 case 0 ->
-                        g2.drawOval(objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1]);
+                        g2.drawOval(objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3]);
                 case 1 ->
-                        g2.drawImage(decals[i], objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1], null);
+                        g2.drawImage(decals[i], objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3], null);
                 case 2 ->
-                        g2.drawLine(objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1]);
+                        g2.drawLine(objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3]);
                 case 3 ->
-                        g2.drawString(strings[i], objects[i][0][0] - camX, objects[i][0][1] - camY);
+                        g2.drawString(strings[i], objects[i][0] - camX, objects[i][1] - camY);
                 case 4 ->
-                        g2.drawRect(objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1]);
+                        g2.drawRect(objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3]);
                 case 5 ->
-                        g2.fillOval(objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1]);
+                        g2.fillOval(objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3]);
                 case 6 ->
-                        g2.fillRect(objects[i][0][0] - camX, objects[i][0][1] - camY, objects[i][1][0], objects[i][1][1]);
+                        g2.fillRect(objects[i][0] - camX, objects[i][1] - camY, objects[i][2], objects[i][3]);
             }
         }
 
         for (int i = 0; i < UIObjects.length; i++) {
-            g2.setColor(new Color(UIObjects[i][2][0], UIObjects[i][2][1], UIObjects[i][2][2]));
-            switch (UIObjects[i][3][0]) {
+            g2.setColor(new Color(UIObjects[i][4], UIObjects[i][5], UIObjects[i][6]));
+            switch (UIObjects[i][7]) {
                 case 0 ->
-                        g2.drawOval(UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1]);
+                        g2.drawOval(UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3]);
                 case 1 ->
-                        g2.drawImage(UIDecals[i], UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1], null);
+                        g2.drawImage(UIDecals[i], UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3], null);
                 case 2 ->
-                        g2.drawLine(UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1]);
+                        g2.drawLine(UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3]);
                 case 3 ->
-                        g2.drawString(UIStrings[i], UIObjects[i][0][0], UIObjects[i][0][1]);
+                        g2.drawString(UIStrings[i], UIObjects[i][0], UIObjects[i][1]);
                 case 4 ->
-                        g2.drawRect(UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1]);
+                        g2.drawRect(UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3]);
                 case 5 ->
-                        g2.fillOval(UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1]);
+                        g2.fillOval(UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3]);
                 case 6 ->
-                        g2.fillRect(UIObjects[i][0][0], UIObjects[i][0][1], UIObjects[i][1][0], UIObjects[i][1][1]);
+                        g2.fillRect(UIObjects[i][0], UIObjects[i][1], UIObjects[i][2], UIObjects[i][3]);
             }
         }
     }

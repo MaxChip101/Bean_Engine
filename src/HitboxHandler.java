63c9c933216747e2ec8cf343bff87ed392b32114
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2023-11-19 21:53:45 -0500

    Update HitboxHandler.java

--- src/HitboxHandler.java
+++ src/HitboxHandler.java
@@ -3,32 +3,18 @@ import java.util.ArrayList;
 
 public class HitboxHandler {
 
-    ArrayList<Rectangle> hitboxes = new ArrayList<>();
+    public ArrayList<BeanObj> objects;
 
-    public int AddHitbox(Rectangle rectangle) {
-        hitboxes.add(rectangle);
-        return hitboxes.indexOf(rectangle);
+    HitboxHandler() {
+        objects = new ArrayList<>();
     }
 
-    public int EditHitbox(int hitboxID, Rectangle rectangle) {
-        hitboxes.add(hitboxID, rectangle);
-        return hitboxID;
-    }
-
-    public Rectangle GetHitboxFromID(int hitboxID) {
-        return hitboxes.get(hitboxID);
-    }
-
-    public void RemoveHitbox(int hitboxID) {
-        hitboxes.remove(hitboxID);
-    }
-
-    public int CheckCollisions(int hitboxID) {
-        for (int i = 0; i < hitboxes.size(); i++) {
-            if (i != hitboxID && hitboxes.get(hitboxID).intersects(hitboxes.get(i))) {
-                return i;
+    public BeanObj CheckCollisions(BeanObj object) {
+        for (int i = 0; i < objects.size(); i++) {
+            if (objects.get(i) != object && object.hitbox.intersects(objects.get(i).hitbox)) {
+                return objects.get(i);
             }
         }
-        return hitboxID;
+        return object;
     }
 }

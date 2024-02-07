fb8d934eb3db71aff090dea411551251fe9f1f68
MaxChip <117135144+MaxChip101@users.noreply.github.com>
2024-02-07 08:02:14 -0500

    Update GraphicsHandler.java

--- src/GraphicsHandler.java
+++ src/GraphicsHandler.java
@@ -3,6 +3,9 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.geom.AffineTransform;
 import javax.swing.*;
+import java.awt.geom.PathIterator;
+import java.awt.geom.Point2D;
+import java.awt.geom.Rectangle2D;
 import java.util.ArrayList;
 import java.util.Comparator;
 
@@ -29,7 +32,7 @@ public class GraphicsHandler extends JPanel implements ActionListener {
         objects.sort(Comparator.comparingInt(BeanObj::getZindex).reversed());
         // draws object graphics
         for (int i = 0; i < objects.size(); i++) {
-            if (currentScene == objects.get(i).scene) {
+            if (currentScene == objects.get(i).scene && objects.get(i).visible){
                 // sets the color of the object to the color to draw
                 g2.setColor(objects.get(i).color);
                 // sets the thickness of the line
@@ -40,25 +43,26 @@ public class GraphicsHandler extends JPanel implements ActionListener {
                 AffineTransform tx = new AffineTransform();
                 tx.concatenate(g2.getTransform());
                 double objRotation = Math.toRadians(objects.get(i).rotation);
-                double rotationX = objects.get(i).bounds.x + objects.get(i).bounds.width / 2.0; // Calculate the center X
-                double rotationY = objects.get(i).bounds.y + objects.get(i).bounds.height / 2.0; // Calculate the center Y
+                double rotationX = objects.get(i).polygon.xpoints[0] + objects.get(i).polygon.xpoints[1] / 2.0; // Calculate the center X
+                double rotationY = objects.get(i).polygon.ypoints[0] + objects.get(i).polygon.ypoints[1] / 2.0; // Calculate the center Y
                 tx.rotate(objRotation, rotationX, rotationY);
                 tx.translate(-objects.get(i).rotationOffset.x, -objects.get(i).rotationOffset.y);
                 g2.setTransform(tx);
 
                 switch (objects.get(i).shape) {
-                    case "oval" -> g2.drawOval(objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height);
-                    case "img" -> g2.drawImage(objects.get(i).image, objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height, null);
-                    case "ln" -> g2.drawLine(objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height);
+                    case "oval" -> g2.drawOval(objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1]);
+                    case "img" -> g2.drawImage(objects.get(i).image, objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1], null);
+                    case "ln" -> g2.drawLine(objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1]);
                     case "str" -> {
                         if (objects.get(i).font != null) {
                             g2.setFont(objects.get(i).font);
                         }
-                        g2.drawString(objects.get(i).string, objects.get(i).bounds.x, objects.get(i).bounds.y);
+                        g2.drawString(objects.get(i).string, objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0]);
                     }
-                    case "rect" -> g2.drawRect(objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height);
-                    case "fillOval" -> g2.fillOval(objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height);
-                    case "fillRect" -> g2.fillRect(objects.get(i).bounds.x, objects.get(i).bounds.y, objects.get(i).bounds.width, objects.get(i).bounds.height);
+                    case "rect" -> g2.drawRect(objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1]);
+                    case "fillOval" -> g2.fillOval(objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1]);
+                    case "fillRect" -> g2.fillRect(objects.get(i).polygon.xpoints[0], objects.get(i).polygon.ypoints[0], objects.get(i).polygon.xpoints[1], objects.get(i).polygon.ypoints[1]);
+                    case "poly" -> g2.fillPolygon(objects.get(i).polygon);
                 }
                 g2.setTransform(originalTransform);
             }
